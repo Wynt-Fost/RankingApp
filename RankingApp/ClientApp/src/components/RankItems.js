251de3e475b328974f39ebbbeb74def1c8bf237c
Akeem <akeemfoster2@gmail.com>
2023-03-06 13:54:14 -0500

    second  commit

--- RankingApp/ClientApp/src/components/RankItems.js
+++ RankingApp/ClientApp/src/components/RankItems.js
@@ -3,13 +3,25 @@
 const RankItems = () => {
 
     const [items, setItems] = useState([]);
+    const dataType = 1;
+
+    useEffect(() => {
+        fetch(`tem/${dataType}`)
+            .then((results) => {
+                return results.json();
+            })
+            .then(data => {
+                setItems(data);
+            })
+    },[]);
 
 
     return (
         <main>
-            (items != null)<div>
-
-            </div>
-        </main>
+            {
+                (items != null) ? items.map((item) => <h3>{item.title} </h3>) : <div>Loading...</div>
+            }
+            
+            </main>
     )
 }
